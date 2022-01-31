3b29662ec203dc8d1980142edd3b6e6db68856c9
Hazel Wu <tinazx056@gmail.com>
2022-01-31 12:26:44 +0800

    feat(setup): initial project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const bcrypt = require('bcryptjs')
+
+const app = express()
+const PORT = 10335
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+  console.log(`應用程式成功在 http://localhost:${PORT} 執行中...`)
+})
