91949a2309d67b3285c2398d6fccf7ea64333fd6
Hazel Wu <tinazx056@gmail.com>
2022-02-01 00:34:27 +0800

    feat(page): Add Detail, Index hbs

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch(error => { return res.status(422).json(error) })
 })
 
 app.get('/users/login', (req, res) => {
@@ -34,13 +39,33 @@ app.get('/users/register', (req, res) => {
 app.post('/users/register', async (req, res) => {
   const { name, email, password, confirmPassword } = req.body
 
-  try {
-    await User.create({ name, email, password })
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
 
-  } catch (error) {
-    console.error(error)
-  }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({ name, email, password }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 
+})
+
+app.get('/todos/:id', (req, res) => {
+  const { id } = req.params
+  return Todo.findByPk(id)
+    // 資料轉換成 plain Object 使用：todo.toJSON() 
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
 })
 
 app.get('/users/logout', (req, res) => {
